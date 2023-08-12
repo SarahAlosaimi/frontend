96fd3cbc450ceff49845e64d4f112cfe34451b3d
Sarah alotaibi <Sarah.as2191@gmail.com>
2023-08-12 16:53:31 +0300

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,59 @@
+import Button from 'react-bootstrap/Button';
+import thiqahLogo from '../assets/thiqahLogo.png';
+import { useState } from "react";
+import {  useNavigate } from "react-router-dom";
+
+import './css/HeaderFooter.css'
+export default function Header() 
+{
+    const [isLoggedIn, setIsLoggedIn] = useState(localStorage.getItem('loggedIn') === 'true');
+    const navigate = useNavigate(); // Initialize the navigate function
+    const [isHovered, setIsHovered] = useState(false);
+
+
+    const handleLogout = () => {
+        localStorage.removeItem('loggedIn');
+        setIsLoggedIn(false);
+        navigate('/login'); // Redirect to login page
+      }
+
+
+
+
+
+const style = {
+    logout:
+{ 
+    background: isHovered ? "#0387b0": "#04ade2",
+    border: isHovered ? "#0387b0" : "#04ade2",
+    transition: "background 0.3s, border 0.3s", // Add smooth transition
+    margin: "10px 20px",
+}, 
+logo :
+{
+    width: "70px" ,
+     height: "60px" ,  
+     margin: "10px 20px",
+
+}
+
+
+}
+
+
+    return (
+        <nav class="navbar navbar-light bg-light">
+        <img src={thiqahLogo}  alt="logo" style={style.logo}l />
+        <Button className="Logout" style = {style.logout}  onClick={handleLogout}   onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)} >Log out</Button>
+
+      </nav>
+    ); 
+
+
+
+
+
+
+
+}
